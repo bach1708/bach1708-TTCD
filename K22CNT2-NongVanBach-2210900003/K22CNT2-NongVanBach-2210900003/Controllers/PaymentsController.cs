f85ad2a87b9a9a637710c20fd1b3f4c9e5d9443f
bach1708 <kunkontkls@gmail.com>
2024-10-31 15:52:44 +0700

    first commit

---
+++ K22CNT2-NongVanBach-2210900003/K22CNT2-NongVanBach-2210900003/Controllers/PaymentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using K22CNT2_NongVanBach_2210900003.Models;
+
+namespace K22CNT2_NongVanBach_2210900003.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Payments
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Booking);
+            return View(payments.ToList());
+        }
+
+        // GET: Payments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ma_dat_ve = new SelectList(db.Bookings, "ma_dat_ve", "ghe_ngoi");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ma_giao_dich,ma_dat_ve,phuong_thuc_thanh_toan,so_tien,trang_thai_thanh_toan,ngay_thanh_toan")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ma_dat_ve = new SelectList(db.Bookings, "ma_dat_ve", "ghe_ngoi", payment.ma_dat_ve);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ma_dat_ve = new SelectList(db.Bookings, "ma_dat_ve", "ghe_ngoi", payment.ma_dat_ve);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ma_giao_dich,ma_dat_ve,phuong_thuc_thanh_toan,so_tien,trang_thai_thanh_toan,ngay_thanh_toan")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ma_dat_ve = new SelectList(db.Bookings, "ma_dat_ve", "ghe_ngoi", payment.ma_dat_ve);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payment payment = db.Payments.Find(id);
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ K22CNT2-NongVanBach-2210900003/K22CNT2-NongVanBach-2210900003/Controllers/ShowtimesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using K22CNT2_NongVanBach_2210900003.Models;
+
+namespace K22CNT2_NongVanBach_2210900003.Controllers
+{
+    public class ShowtimesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Showtimes
+        public ActionResult Index()
+        {
+            var showtimes = db.Showtimes.Include(s => s.Cinema).Include(s => s.Movy);
+            return View(showtimes.ToList());
+        }
+
+        // GET: Showtimes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showtime showtime = db.Showtimes.Find(id);
+            if (showtime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showtime);
+        }
+
+        // GET: Showtimes/Create
+        public ActionResult Create()
+        {
+            ViewBag.ma_rap = new SelectList(db.Cinemas, "ma_rap", "ten_rap");
+            ViewBag.ma_phim = new SelectList(db.Movies, "ma_phim", "ten_phim");
+            return View();
+        }
+
+        // POST: Showtimes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ma_suat_chieu,ma_phim,ma_rap,ngay_chieu,gio_chieu,phong_chieu")] Showtime showtime)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Showtimes.Add(showtime);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ma_rap = new SelectList(db.Cinemas, "ma_rap", "ten_rap", showtime.ma_rap);
+            ViewBag.ma_phim = new SelectList(db.Movies, "ma_phim", "ten_phim", showtime.ma_phim);
+            return View(showtime);
+        }
+
+        // GET: Showtimes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showtime showtime = db.Showtimes.Find(id);
+            if (showtime == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ma_rap = new SelectList(db.Cinemas, "ma_rap", "ten_rap", showtime.ma_rap);
+            ViewBag.ma_phim = new SelectList(db.Movies, "ma_phim", "ten_phim", showtime.ma_phim);
+            return View(showtime);
+        }
+
+        // POST: Showtimes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ma_suat_chieu,ma_phim,ma_rap,ngay_chieu,gio_chieu,phong_chieu")] Showtime showtime)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(showtime).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ma_rap = new SelectList(db.Cinemas, "ma_rap", "ten_rap", showtime.ma_rap);
+            ViewBag.ma_phim = new SelectList(db.Movies, "ma_phim", "ten_phim", showtime.ma_phim);
+            return View(showtime);
+        }
+
+        // GET: Showtimes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Showtime showtime = db.Showtimes.Find(id);
+            if (showtime == null)
+            {
+                return HttpNotFound();
+            }
+            return View(showtime);
+        }
+
+        // POST: Showtimes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Showtime showtime = db.Showtimes.Find(id);
+            db.Showtimes.Remove(showtime);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
